63dcd621b08a3fdd4726de0fe4d1835fa55e334e
gonzalonunezos <gonzalo.bbs@gmail.com>
2016-11-08 08:40:01 -0400

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,1 +1,26 @@
 print "Hola"
+# Buscar el minimo comun multiplo entre 2 numeros
+# define a function
+def lcm(x, y):
+   """Esta función recibe dos enteros y devuelve el multiplo comum"""
+ 
+   # cogemos el mayor de los dos
+   if x > y:
+       mayor = x
+   else:
+       mayor = y
+ 
+   while(True):
+       if((mayor % x == 0) and (mayor % y == 0)):
+           lcm = mayor
+           break
+       mayor += 1
+ 
+   return lcm
+ 
+ 
+# solicitamos los dos numeros
+num1 = int(input("Introduce el primer numero: "))
+num2 = int(input("Introduce el segundo numero: "))
+ 
+print("El minimo comun multiplo de ", num1," y ", num2," es ", lcm(num1, num2))
